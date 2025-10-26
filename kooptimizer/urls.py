089bdc1a70c0d830617c749cc5cc8dbb0c4467fb
Art <delacionhearty@gmail.com>
2025-10-26 19:49:46 +0800

    Revert "Integrate Database on Login and Add SMS 2FA API Connection"

--- kooptimizer/urls.py
+++ kooptimizer/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
-from apps.users import views as user_views
+from django.urls import path
+from apps.users import views as user_views  # This imports new view
 from apps.home import views as home_views
 from apps.core import views as core_views
 from django.conf import settings
@@ -8,16 +8,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # This is new homepage URL:
+    # It says when a user visits the root path (''),
+    # run the 'login_view' function from user_views file.
     path('', user_views.login_view, name='login'), 
     path('home/', home_views.home_view, name='home'),
     path('download/', core_views.download_view, name='download'),
     path('about/', core_views.about_view, name='about'),
-    
-    # User-related URLs (login, logout, first-login-setup, etc.)
-    path('users/', include('apps.users.urls')),
-
-    # Dashboard URLs
-    path('dashboard/', include('apps.dashboard.urls', namespace='dashboard')),
 
 
 ]
--- stored_procedures/enums.sql
+++
@@ -1,3 +0,0 @@
-CREATE TYPE gender_enum AS ENUM ('male', 'female');
-CREATE TYPE user_role_enum AS ENUM ('admin', 'staff', 'officer');
-CREATE TYPE verification_status_enum AS ENUM ('pending', 'verified');
