6005614496dda0a786c8d8ed7b583d386c558b98
Art <delacionhearty@gmail.com>
2025-11-14 16:27:36 +0800

    Fixed

--- apps/account_management/urls.py
+++ apps/account_management/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'account_management'
 
 urlpatterns = [
-     path('account_management/', views.account_management, name='account_management'),
+    # This is the path for your main page
+    path('account_management/', views.account_management, name='account_management'),
+    
+    # ADD THIS NEW LINE for the API
+    path('api/send-credentials/', views.send_credentials_view, name='send_credentials'),
 ]
